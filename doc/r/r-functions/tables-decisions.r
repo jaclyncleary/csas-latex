9de1dc54377c831b8ec5e5942426219086d446db
Chris Grandin <cgrandin@shaw.ca>
2017-04-12 12:13:31 -0700

    Add files necessary for previous few commits to work.

---
+++ doc/r/r-functions/tables-decisions.r
@@ -0,0 +1,32 @@
+make.decision.table <- function(model,
+                                digits = 3,
+                                xcaption = "default",
+                                xlabel   = "default",
+                                font.size = 9,
+                                space.size = 10,
+                                placement = "H"){
+  ## Returns an xtable
+  ##
+  ## digits - number of decimal places for the values
+  ## xcaption - caption to appear in the calling document
+  ## xlabel - the label used to reference the table in latex
+  ## font.size - size of the font for the table
+  ## space.size - size of the vertical spaces for the table
+  ## placement - latex code for placement of the table in document
+
+  tab <- model$mcmccalcs$proj.dat
+  ## Don't format the first column as it should be the TAC
+  tab[, -1] <- f(tab[, -1], digits)
+
+  size.string <- latex.size.str(font.size, space.size)
+  print(xtable(tab,
+               caption = xcaption,
+               label = xlabel,
+               align = getAlign(ncol(tab))),
+        caption.placement = "top",
+        include.rownames = FALSE,
+        sanitize.text.function = function(x){x},
+        size = size.string,
+        table.placement = placement,
+        booktabs = TRUE)
+}
