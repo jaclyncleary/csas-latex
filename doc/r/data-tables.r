4231b04b1f6752e66220c4ef07fda190dda45e18
Chris Grandin <cgrandin@shaw.ca>
2017-06-20 07:59:40 -0700

    Add text on 'Biological background' and data file for the biological data.

--- doc/r/data-tables.r
+++ doc/r/data-tables.r
@@ -9,7 +9,7 @@ survey.history.file <- "survey-history.csv"
 sens.params.file <- "sensitivity-descriptions.csv"
 trawl.bio.file <- "trawl-obs-len-wt-age.csv"
 gear.names.file <- "gear-names.csv"
-bio.file <- "bio.csv"
+bio.file <- "bio.dat"
 
 cat("Loading all data tables (csv files) from ", data.path, "\n")
 catches <- load.csv(data.path, catch.data.file)
@@ -17,5 +17,7 @@ survey.history <- load.csv(data.path, survey.history.file)
 sens.desc <- load.csv(data.path, sens.params.file)
 trawl.bio <- load.csv(data.path, trawl.bio.file)
 gear.names <- load.csv(data.path, gear.names.file, header = FALSE)
-bio <- load.csv(data.path, bio.file)
+## bio is an ascii file which was saved from iscam-gui using:
+## save(bio, "bio.dat", ascii = TRUE)
+load(file.path(data.path, bio.file))
 cat("All data tables have been loaded ", data.path, "\n")
