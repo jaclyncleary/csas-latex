564604f2440b05d06bf180b66c579e97760083a5
Chris Grandin <cgrandin@shaw.ca>
2017-06-27 07:49:37 -0700

    Add prior variables for steepness in the custom-knitr-variables.r and make changes to the priors section in the doc.

--- doc/r/custom-knitr-variables.r
+++ doc/r/custom-knitr-variables.r
@@ -1,6 +1,11 @@
 ## Put any variables you intend to use in the text here.
 ## The function f() is for formatting and is defined in
 ##  r-functions/utilities.r
+##
+## The variables defined here depend on the tructure of the
+##  model-setup.r source code.
+
+b <- base.model[[1]]
 
 ################################################################################
 ## Parameter values
@@ -17,7 +22,6 @@ fmsy.hi <- r.quants["fmsy", 4]
 
 ################################################################################
 ## Number of mcmc samples, min and max median biomass
-b <- base.model[[1]]
 num.mcmc.samples <- nrow(b$mcmc$params)
 median.bio.min  <- f(min(b$mcmccalcs$sbt.quants[2,]), 3)
 median.bio.min.year <- names(which.min(min(b$mcmccalcs$sbt.quants[2,])))
@@ -26,14 +30,40 @@ median.bio.max.year <- names(which.max(b$mcmccalcs$sbt.quants[2,]))
 
 ################################################################################
 ## Priors settings from the control file
-base.m.prior.mean <- f(exp(base.model[[1]]$ctl$params[3, 6]), 1)
-base.m.prior.sd <- f(base.model[[1]]$ctl$params[3, 7], 2)
-sens.7.m.prior.mean <- f(exp(sens.models.4[[1]]$ctl$params[3, 6]), 1)
-sens.7.m.prior.sd <- f(sens.models.4[[1]]$ctl$params[3, 7], 2)
-sens.8.m.prior.mean <- f(exp(sens.models.4[[2]]$ctl$params[3, 6]), 1)
-sens.8.m.prior.sd <- f(sens.models.4[[2]]$ctl$params[3, 7], 2)
-sens.9.m.prior.mean <- f(exp(sens.models.5[[1]]$ctl$params[3, 6]), 1)
-sens.9.m.prior.sd <- f(sens.models.5[[1]]$ctl$params[3, 7], 2)
+priors <- as.data.frame(b$ctl$params)
+s7.priors <- as.data.frame(sens.models.4[[1]]$ctl$params)
+s8.priors <- as.data.frame(sens.models.4[[2]]$ctl$params)
+s9.priors <- as.data.frame(sens.models.5[[1]]$ctl$params)
+
+## M priors
+base.m.prior <- priors[rownames(priors) == "log_m",]
+base.m.prior.mean <- f(exp(base.m.prior$p1), 1)
+base.m.prior.sd <- f(base.m.prior$p2, 2)
+
+s7.prior <- s7.priors[rownames(s7.priors) == "log_m",]
+sens.7.m.prior.mean <- f(exp(s7.prior$p1), 1)
+sens.7.m.prior.sd <- f(s7.prior$p2, 2)
+
+s8.prior <- s8.priors[rownames(s8.priors) == "log_m",]
+sens.8.m.prior.mean <- f(exp(s8.prior$p1), 1)
+sens.8.m.prior.sd <- f(s8.prior$p2, 2)
+
+s9.prior <- s9.priors[rownames(s9.priors) == "log_m",]
+sens.9.m.prior.mean <- f(exp(s9.prior$p1), 1)
+sens.9.m.prior.sd <- f(s9.prior$p2, 2)
+
+## Steepness prior
+h.prior <- priors[rownames(priors) == "h",]
+h.prior.alpha <- h.prior$p1
+h.prior.beta <- h.prior$p2
+h.prior.mean <- h.prior.alpha / (h.prior.alpha + h.prior.beta)
+h.prior.cv <- f(sqrt((h.prior.alpha * h.prior.beta) /
+                   ((h.prior.alpha + h.prior.beta)^2 *
+                    (h.prior.alpha + h.prior.beta + 1))) /
+                h.prior.mean, 2)
+h.prior.alpha <- f(h.prior.alpha, 1)
+h.prior.beta <- f(h.prior.beta, 1)
+h.prior.mean <- f(h.prior.mean, 2)
 
 ################################################################################
 ## Age comp data values
@@ -43,20 +73,20 @@ trawl.gear <- 1
 qcsss.gear <- 2
 hsss.gear <- 3
 wcviss.gear <- 4
-trawl.yrs <- sort(unique(base.model[[1]]$dat$age.comps[[trawl.gear]][,1]))
+trawl.yrs <- sort(unique(b$dat$age.comps[[trawl.gear]][,1]))
 trawl.yrs <- paste0(min(trawl.yrs), "--", max(trawl.yrs))
-qcsss.yrs <- sort(unique(base.model[[1]]$dat$age.comps[[qcsss.gear]][,1]))
+qcsss.yrs <- sort(unique(b$dat$age.comps[[qcsss.gear]][,1]))
 qcsss.yrs <- paste(qcsss.yrs, collapse=", ")
-hsss.yrs <- sort(unique(base.model[[1]]$dat$age.comps[[hsss.gear]][,1]))
+hsss.yrs <- sort(unique(b$dat$age.comps[[hsss.gear]][,1]))
 hsss.yrs <- paste(hsss.yrs, collapse=", ")
-wcviss.yrs <- sort(unique(base.model[[1]]$dat$age.comps[[wcviss.gear]][,1]))
+wcviss.yrs <- sort(unique(b$dat$age.comps[[wcviss.gear]][,1]))
 wcviss.yrs <- paste(wcviss.yrs, collapse=", ")
 
 ################################################################################
 ## Values for posteriors
-base.quants <- as.data.frame(base.model[[1]]$mcmccalcs$p.quants)
+base.quants <- as.data.frame(b$mcmccalcs$p.quants)
 base.m.quants <- f(base.quants$m, 3)
-base.bo.quants <- f(1000 * base.model[[1]]$mcmccalcs$r.quants[1, 2:4])
+base.bo.quants <- f(1000 * b$mcmccalcs$r.quants[1, 2:4])
 
 sens.7.m.quants <- f(as.data.frame(sens.models.4[[1]]$mcmccalcs$p.quants)$m, 3)
 sens.7.bo.quants <- f(1000 * sens.models.4[[1]]$mcmccalcs$r.quants[1, 2:4])
@@ -66,12 +96,12 @@ sens.9.m.quants <- f(as.data.frame(sens.models.5[[1]]$mcmccalcs$p.quants)$m, 3)
 
 ################################################################################
 ## Catch data values
-catch.yrs <- sort(unique(base.model[[1]]$dat$catch[,1]))
+catch.yrs <- sort(unique(b$dat$catch[,1]))
 catch.yrs <- paste0(min(catch.yrs), "--", max(catch.yrs))
 
 ################################################################################
 ## Calculation of sigma and tau from rho and vartheta
-rho <- base.model[[1]]$ctl$params[6, 1]
-vartheta <- base.model[[1]]$ctl$params[7, 1]
+rho <- b$ctl$params[6, 1]
+vartheta <- b$ctl$params[7, 1]
 tau <- f(sqrt((1 - rho) / vartheta), 1)
 sigma <- f(sqrt(rho / vartheta), 1)
