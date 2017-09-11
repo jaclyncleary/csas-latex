702f6672817de917818cb6e79b2e21aa742f173a
Chris Grandin <cgrandin@shaw.ca>
2017-09-11 12:53:18 -0700

    Add parameter tables for first two stocks. See issue #3.

--- doc/r/all.r
+++ doc/r/all.r
@@ -44,8 +44,8 @@ source(file.path(func.dir, "mcmc-diagnostics.r"))
 ## source(file.path(func.dir, "read-list.r"))
 
 source(file.path(func.dir, "tables-activities-management.r"))
+source(file.path(func.dir, "tables-parameters.r"))
 
-## source(file.path(func.dir, "tables-parameters.r"))
 ## source(file.path(func.dir, "tables-decisions.r"))
 ## source(file.path(func.dir, "tables-data.r"))
 ## source(file.path(func.dir, "figures-catch.r"))
