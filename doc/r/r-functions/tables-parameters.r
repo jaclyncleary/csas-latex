12fd63115549bff6b3a84bdaedf5a6d70bbe934f
Chris Grandin <cgrandin@shaw.ca>
2017-09-19 10:55:02 -0700

    Add recruitment posterior tables for all stocks, also add syr argument to table code so they can start at any year.

--- doc/r/r-functions/tables-parameters.r
+++ doc/r/r-functions/tables-parameters.r
@@ -385,6 +385,7 @@ make.ref.points.table <- function(model,
 
 make.value.table <- function(model,
                              type,
+                             syr,
                              digits = 3,
                              xcaption = "default",
                              xlabel   = "default",
@@ -394,7 +395,7 @@ make.value.table <- function(model,
                              tabular.environment = "tabular"){
   ## Returns an xtable in the proper format for values (biomasas, recr, etc)
   ##
-  ## out.dat - one of the quants objects as output by the calc.mcmc function
+  ## syr - start year for table data
   ## type - 1=biomass, 2=recruitment, 3=F, 4=U, 5=depletion
   ## digits - number of decimal places for the values
   ## xcaption - caption to appear in the calling document
@@ -426,6 +427,7 @@ make.value.table <- function(model,
 
   tab <- f(t(out.dat), digits)
   tab <- cbind(rownames(tab), tab)
+  tab <- tab[tab[,1] >= syr,]
   col.names <- colnames(tab)
   col.names[1] <- "Year"
   col.names <- latex.bold(latex.perc(col.names))
