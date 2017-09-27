f11d58e02961a650bc0c752f4998bacae3e8ab6a
Chris Grandin <cgrandin@shaw.ca>
2017-09-27 14:01:22 -0700

    Add recruitment devs figures for sensitivities to M. See issue #20.

--- doc/r/all.r
+++ doc/r/all.r
@@ -56,7 +56,7 @@ source(file.path(func.dir, "figures-indices.r"))
 source(file.path(func.dir, "figures-selectivity.r"))
 ## source(file.path(func.dir, "figures-reference-points.r"))
 source(file.path(func.dir, "figures-biomass.r"))
-## source(file.path(func.dir, "figures-recruitment.r"))
+source(file.path(func.dir, "figures-recruitment.r"))
 ## source(file.path(func.dir, "figures-mortality.r"))
 
 ## Code to verify the model setup
