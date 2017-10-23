5e4024d78636112150db9481b7a891cb40401f70
Chris Grandin <cgrandin@shaw.ca>
2017-10-23 09:37:40 -0700

    Add age comp fit and residual plotting functions.

--- doc/r/r-functions/figures-age.r
+++ doc/r/r-functions/figures-age.r
@@ -46,6 +46,94 @@ make.age.comp.estimates.plot <- function(model,
   axis(4)
 }
 
+make.age.comp.fit.plot <- function(model,
+                                   which.gear,
+                                   fg = gray(level = 0.1, alpha = 0.5),
+                                   bg = gray(level = 0.5, alpha = 0.5),
+                                   inches = 0.12){
+
+  fit.dat <- as.data.frame(model$mpd$A_hat)
+  comp.dat <- as.data.frame(model$mpd$d3_A)
+
+  s.age <- model$mpd$n_A_sage[which.gear]
+  n.age <- model$mpd$n_A_nage[which.gear]
+  ages <- s.age:n.age
+  n.ages <- length(s.age:n.age)
+
+
+  ## resid.dat has unlabelled rows, which need to be extracted
+  ##  correctly based on the values in the comp.dat data.frame
+  which.rows <- which(comp.dat[,2] == which.gear)
+  comp.dat <- comp.dat[which.rows ,]
+  yrs <- comp.dat[, 1]
+
+  fit.dat <- fit.dat[which.rows,]
+  ## Remove the information columns, leaving only the comps
+  comp.dat <- as.matrix(comp.dat[, -seq(1, 5)])
+  obs.prop <- prop.table(comp.dat, 1)
+
+  max.y <- max(fit.dat, obs.prop)
+  n.side <- get.rows.cols(length(yrs))
+  par(mfrow = n.side,
+      oma = c(2, 3, 1, 1),
+      mai = c(0.2, 0.4, 0.3, 0.2))
+  for(yr in 1:length(yrs)){
+    year <- yrs[yr]
+    obs <- obs.prop[yr,]
+    est <- fit.dat[yr,]
+    plot(ages,
+         obs,
+         type = "h",
+         xlab = "",
+         ylab = "",
+         main = year,
+         las = 1,
+         ylim = c(0, max.y))
+    lines(ages, est, lty = 1, lwd = 2, col = 2)
+  }
+  mtext("Age", side = 1, line = 0, outer = TRUE)
+  mtext("Proportion", side = 2, line = 1, outer = TRUE)
+}
+
+make.age.comp.residuals.plot <- function(model,
+                                         which.gear,
+                                         fg = gray(level = 0.1, alpha = 0.5),
+                                         bg = gray(level = 0.5, alpha = 0.5),
+                                         inches = 0.12){
+
+  resid.dat <- as.data.frame(model$mpd$A_nu)
+  comp.dat <- as.data.frame(model$mpd$d3_A)
+
+  s.age <- model$mpd$n_A_sage[which.gear]
+  n.age <- model$mpd$n_A_nage[which.gear]
+  ages <- s.age:n.age
+  n.ages <- length(s.age:n.age)
+
+
+  ## resid.dat has unlabelled rows, which need to be extracted
+  ##  correctly based on the values in the comp.dat data.frame
+  which.rows <- which(comp.dat[,2] == which.gear)
+  comp.dat <- comp.dat[which.rows ,]
+  yrs <- comp.dat[, 1]
+
+  resid.dat <- resid.dat[which.rows,]
+  ## Remove the information columns, leaving only the comps
+  comp.dat <- as.matrix(comp.dat[, -seq(1, 5)])
+  obs.prop <- prop.table(comp.dat, 1)
+
+  plotBubbles(t(resid.dat),
+              xval = yrs,
+              yval = ages,
+              size = 0.1,
+              powr = 0.5,
+              xlab = "Year",
+              ylab = "Age",
+              las = 1,
+              cex = 1.25,
+              axes = FALSE)
+  axis(1, at = yrs, labels = yrs)
+}
+
 make.age.comp.data.plot <- function(model,
                                     which.gear){
 
