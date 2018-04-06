ecd22b15e681f8c3024251f4ca91296dd2002073
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2018-04-06 13:02:35 -0700

    added MWE to use with csas-style frontmatter branch

---
+++ MWE/clean.bat
@@ -0,0 +1,18 @@
+# Clean up the directory after a LaTeX build. Windows version
+del *.aux
+del *.bbl
+del *.blg
+del *.dvi
+del *.log
+del *.lot
+del *.lof
+del *.ps
+del *.toc
+del *.txt
+del *.out
+del *.snm
+del *.nav
+del *-concordance.tex
+del *.synctex.gz*
+del res-docMSE.tex
+del res-docMSE.pdf
